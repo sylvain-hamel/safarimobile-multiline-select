ce06ad8d1f9c092d0c314a27536008f0023fa7a6
RedHotSly <rhsgh0@gmail.com>
2012-07-18 22:45:06 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,25 @@
+#Problem
+Safari for mobile ignores the 'size' attribute of \<select\> elements. It always renders a one-line high drop down list instead of a multiline list box.
+
+#Solution
+If the browser is not Safari for mobile, this plugin hides the \<select\> and replaces it by something that looks and acts like a list. 
+
+
+#Usage
+
+##Code:
+
+	$().ready(function () {
+		$("select").fixForSafariMobile();
+	})
+
+## Style for height and width
+The plugin is not able to set a good value for the height and width of the list it creates (send a pull request if you fix this). For now, you must define styles to set a correct height and width. The plugin generates an 'id' for the replacement list based on the original select's id by appending '_safarimobile' to the name. You should defne styles like this:
+
+    #select1_safarimobile { height: 200px;  width:200px; } 
+    #select2_safarimobile { height: 100px;  width:150px; } 
+
+#Details
+* The original \<select\> is not removed from the DOM so existing code can still change or access its value. The plugin keeps its valiu in sync with the replacement list's current value.
+* The plugin is uses hardcoded styles that are injected in the DOM. I did that to ship the plugin as a single file. You can change the source code or just add styles to your stylesheet to override the plugin's default styles.
+*  It only works for single selection (send a pull request if you need multiple selection support).
---
+++ src/safarimobile-multiline-select.js
@@ -0,0 +1,141 @@
+/* 
+===============================================================
+jQuery plugin to expand/collapse a content element when a 
+expander element is clicked. When expanding/collapsing the plug-in 
+also toggles a class on the element.
+See https://github.com/redhotsly/simple-expand
+===============================================================
+Copyright (C) 2012 Sylvain Hamel
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished 
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+===============================================================
+*/
+/*global $:false, window:false, navigator:false */
+(function () {
+    "use strict";
+
+    // SimpleExpand 
+    function SelectConverter() {
+        
+        // set this to true to bypass user agent check when 
+        // testing in a browser other than safari mobile 
+        var testMode = true; 
+
+        var that = this;
+
+        var styles =
+        '<style type="text/css">' +
+        '    /* styles for the safarimobile-multiline-select plug-in */ \r\n'  +
+        '    .multilineselect {border: 1px solid silver; display:inline-block; margin:0px; padding:2px; height: 100px; overflow:auto; width:300px; vertical-align: text-bottom;} \r\n' +
+        '    .multilineselect li {list-style-type: none; list-style-position:inside; margin:0px; padding:0px; cursor:default ; }  \r\n' + 
+        '    .multilineselect li a {text-decoration: none; color: black; width: 100%:display:inline-block;}  \r\n' +
+        '    .multilineselect li.selected {color: white; background-color: darkgrey;}  \r\n' +
+        '    .multilineselect li.selected a {color: white; background-color: darkgrey;}  \r\n' +
+        '</style>';
+
+
+        that.isSafariMobile = function () {
+            return (navigator.userAgent.match(/(iPod|iPhone|iPad)/) && navigator.userAgent.match(/AppleWebKit/)) || testMode;
+        };
+
+        that.selectIsMultiline = function (item) {
+            var value = item.attr('size');
+            return  !( value === undefined || value === "1");
+        };
+
+        that.selectValue = function(ul, val){
+            ul.children('li').removeClass('selected');
+            var toSelect = ul.find('li[data-value="' + val + '"]');
+            toSelect.addClass('selected');
+        };
+
+        that.createListFromSelectElement = function (select) {
+            var ul = $('<ul class="multilineselect">');
+
+            var selectid = select.attr("id");
+            if ( selectid !== undefined ){
+                ul.attr("id", selectid + "_safarimobile")    
+            }
+
+            select.children('option').each(function (option) {
+                var $option = $(this);
+                
+                var li = $('<li>');
+                li.attr('data-value', $option.val());
+                li.html($option.html());
+
+                // when items is clicked, push value to the original <select>
+                li.click(function() {
+                    var value = $(this).attr('data-value');
+                    select.val(value);
+                    select.change();                   
+                });
+
+                ul.append(li);
+            });
+
+            // when the <select> value change, select the corresponding item in the list
+            select.change(function () {
+                var selected = $(this).children('option:selected');
+                that.selectValue(ul, selected.val());
+            });
+
+            return ul;
+        };
+
+        that.fixForSafariMobile = function (jquery) {
+            if (!that.isSafariMobile())
+            {
+                return;
+            }
+
+            $("head").prepend(styles);
+
+            // Plug-in entry point
+            jquery.each(function () {
+                var select = $(this);
+
+                // hide the select element but keep it in the DOM to allow existing code to
+                // keep on binding to it 
+                select.hide();
+
+                if ( !that.selectIsMultiline(select))
+                {
+                    return;
+                }
+
+                var newlist = that.createListFromSelectElement(select);
+
+                select.after(newlist);
+                
+            });
+        };
+    }
+
+    // export SelectConverter (to instantiate in unit tests)
+    window.SelectConverter = SelectConverter;
+
+    // expose SelectConverter as a jQuery plugin
+    $.fn.fixForSafariMobile = function () {
+        var instance = new SelectConverter();
+        instance.fixForSafariMobile(this);
+        return this;
+    };
+}());
